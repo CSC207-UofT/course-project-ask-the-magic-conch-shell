424d6b229d13ad2cb12d2869e8d583df05a0f2ab
Lanmanc <fucheng.zhuang@mail.utoronto.ca>
2021-10-14 19:15:29 -0400

    User_manager

---
+++ src/main/java/Manager/User_Manager.java
@@ -0,0 +1,26 @@
+package Manager;
+import User.User;
+import User.Staff;
+import User.Student;
+
+public class User_Manager {
+    public void create_new_user(){
+        //ToDO
+    }
+    public void delete_user(){
+        //TODO
+    }
+    public void store_all_user(){
+        //TODO
+    }
+    public boolean check_credit_score(){
+        //TODO
+        return true;
+    }
+    public void modify_password(int password){
+        //TODO
+    }
+    public void modify_currently_borrowing_record(){
+        //TODO
+    }
+}
--- src/main/java/User/Student.java
+++ src/main/java/User/Student.java
@@ -1,4 +1,5 @@
 package User;
+import Book_package.Book;
 
 public class Student extends User{
     private int CreditScore;
