f530f64bc73aeffd5341d4edfbaf246cc534deb2
nzhou27 <90351870+nzhou27@users.noreply.github.com>
2021-10-14 17:11:15 -0400

    Update Inventory.java

--- src/main/java/controller/Inventory.java
+++ src/main/java/controller/Inventory.java
@@ -1,8 +1,12 @@
 package controller;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.List; 
 
 public class Inventory {
-    public void Inventory
+    public void  add_book_to_inventor(int book_id, String book_name, String isbn, String book_name, Date published_date, String author, Book_position_status status, Date return_date){
+           }  
+
+    public String delete_book_from_inventory(int book_id){
+}
 }
