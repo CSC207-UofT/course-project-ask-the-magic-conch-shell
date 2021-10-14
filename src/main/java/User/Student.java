dbcda3d13f8f4f6f53cd00bf4efc32b9ca1a066e
Lanmanc <fucheng.zhuang@mail.utoronto.ca>
2021-10-14 18:27:29 -0400

    user

---
+++ src/main/java/User/Student.java
@@ -0,0 +1,29 @@
+package User;
+
+public class Student extends User{
+    private int CreditScore;
+    private Book[] CurrentBorrowingRecords;
+    public void Student(String username){
+        super.User(username);
+        Book[] CurrentBorrowingRecords = new Book[5];
+        this.CurrentBorrowingRecords = CurrentBorrowingRecords;
+
+
+    }
+    public void CreditScoreSetter(int CreditScore){
+        this.CreditScore = CreditScore;
+    }
+    public int CreditScoreGetter(){
+        return this.CreditScore;
+    }
+    public void CurrentBorrowingRecordsSetter(Book book){
+        // TODO
+
+    }
+    public Book[] CurrentBorrowingRecordsGetter(){
+        return this.CurrentBorrowingRecords;
+    }
+
+
+
+}
