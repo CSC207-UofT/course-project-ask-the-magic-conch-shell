aced661b6fd7d562ba2a4ce16d5169f1e0da4267
Mileyz023 <miley.zhang@mail.utoronto.ca>
2021-10-15 16:39:45 -0400

    add ui demo, commandexecutor, ui.

---
+++ src/main/java/UI_demo/UI_demo.java
@@ -0,0 +1,10 @@
+package UI_demo;
+
+public class UI_demo {
+    public static void main(String[] args) {
+        System.out.print("Welcome to the Library.");
+        System.out.print("Please input your username.");
+
+    }
+
+}
---
+++ src/main/java/UI_demo/command_executor.java
@@ -0,0 +1,16 @@
+package UI_demo;
+
+
+
+public class command_executor {
+    public String executeCommand(String commandLine){
+        if (commandLine.equals("")) {
+            return "";
+        }
+        if(commandLine.equals())
+
+
+    }
+
+
+}
