bdb4036939547589bab776efc3f341623a62f107
nzhou27 <zhouhanzhi0818@gmail.com>
2021-10-15 18:52:45 -0400

    UI demo and command_executor.java implemented

--- src/main/java/UI_demo/UI_demo.java
+++ src/main/java/UI_demo/UI_demo.java
@@ -1,9 +1,12 @@
 package UI_demo;
 
+import java.util.Scanner;
+
 public class UI_demo {
     public static void main(String[] args) {
         System.out.print("Welcome to the Library.");
-        System.out.print("Please input your username.");
+        command_executor.log_in();
+
 
     }
 
