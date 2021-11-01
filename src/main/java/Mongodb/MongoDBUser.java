8657c5eb43432278b71da309b37a524ffa97bfdd
Lanmanc <fucheng.zhuang@mail.utoronto.ca>
2021-11-01 14:03:10 -0400

    add mongodb2

--- src/main/java/Mongodb/MongoDBUser.java
+++ src/main/java/Mongodb/MongoDBUser.java
@@ -4,8 +4,12 @@ package Mongodb;
 import com.mongodb.*;
 import org.bson.Document;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+
 
 public class MongoDBUser {
     public static HashMap<String, DBObject> database;
@@ -19,29 +23,31 @@ public class MongoDBUser {
             database = new HashMap<String, DBObject>();
             while (cursor.hasNext()){
                 DBObject data = cursor.next();
-                database.put(data.get("userName").toString(), data);
-                //System.out.println(data.get("userName"));
+                database.put(data.get("username").toString(), data);
             }
+
             System.out.println(database.keySet());
-            System.out.println(database.get("sky").get("passWord"));
+            System.out.println(database.get("sky").get("password"));
+            MongoDBUser.update("sky","1111");
+            System.out.println(database.get("sky").get("password"));
         }catch(Exception e){
             System.out.println(e);
         }
     }
-    public void update(String userName, String passWord){
-        // TODO: 2021-11-01  o
-//        MongoClient mongoclient = new MongoClient("localhost", 27017);
-//        DB db = mongoclient.getDB("local");
-//        System.out.println("MongoDB Connected");
-//        DBCollection coll = db.getCollection("User");
-//        Document found = (Document) coll.find (new DB("userName",userName)).first();
-//
-//        BasicDBObject newDocument = new BasicDBObject();
-//        newDocument.put("hostingName", "goDaddy"); // (2)
-//
-//        BasicDBObject updateObject = new BasicDBObject();
-//        updateObject.put("$set", newDocument); // (3)
-//
-//        db.getCollection("webHostInfo").updateOne(query, updateObject); // (4)
+    public static void update(String userName, String passWord){
+        MongoClient mongoclient = new MongoClient("localhost", 27017);
+        DB db = mongoclient.getDB("local");
+        System.out.println("MongoDB Connected");
+        DBCollection coll = db.getCollection("User");
+
+        BasicDBObject oldData = new BasicDBObject();
+        oldData.put(userName,database.get(userName).get("password"));
+        BasicDBObject newDocument = new BasicDBObject();
+        newDocument.put(userName, passWord); // (2)
+
+        BasicDBObject updateObject = new BasicDBObject();
+        updateObject.put("$set", newDocument); // (3)
+
+        coll.update(oldData, updateObject); // (4)
     }
 }
