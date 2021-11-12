cbde4c940de62d7dd7d23d276848d719902d7193
vannie <vannie.wang@mail.utoronto.ca>
2021-11-12 14:14:15 -0500

    modify bookmanager

--- src/main/java/Book/BookManager.java
+++ src/main/java/Book/BookManager.java
@@ -7,6 +7,11 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Objects;
 
+/**
+ * Manager all Book. Responsible for adding, deleting, searching book,
+ * checking and changing return date and book status.
+ */
+
 public class BookManager {
 
     public static ArrayList<Book> allBooks;
@@ -20,8 +25,19 @@ public class BookManager {
     public BookManager() {
     }
 
-    public static void addBook(Book book) { allBooks.add(book);
+    /**
+     * Adds an instance of Book to the overall list of Books (allBooks)
+     * @param book a book
+     */
+
+    public static void addBook(Book book) {allBooks.add(book);
     }
+
+    /**
+     * Deletes an instance of Book from the overall list of Books by its book ID
+     * @param bookID book ID
+     * @return true if the book has been successfully removed, false otherwise
+     */
 
     public static boolean deleteBook(int bookID) {
         for (Book book : allBooks) {
@@ -33,6 +49,12 @@ public class BookManager {
         return false;
     }
 
+    /**
+     * Search a book by its ISBN
+     * @param book_ISBN a book's ISBN
+     * @return a list of book ids that has the same ISBN as the input book ISBN
+     */
+
     public ArrayList<Integer> searchBook(String book_ISBN) {
         ArrayList<Integer> ids = new ArrayList<>();
         for (Book book : allBooks) {
@@ -41,8 +63,13 @@ public class BookManager {
             }
         }
         return ids;
-
     }
+
+    /**
+     * search books by its author. Return a list of books that are written by author_name
+     * @param author_name author name
+     * @return a hashmap of book names and their corresponding ISBNs
+     */
 
     public HashMap<String, String> searchByAuthor(String author_name) {
         HashMap<String, String> authors = new HashMap<>();
@@ -54,6 +81,13 @@ public class BookManager {
         return authors;
     }
 
+    /**
+     * Search books by their categories (Dictionary, Literature, Magazine, ResearchPaper,
+     * Textbook)
+     * @param book_type book category
+     * @return a hashmap (BookName and its corresponding ISBN) of books that belong
+     * to input book_type category.
+     */
 
     public HashMap<String, String> searchByType(String book_type){
         HashMap<String, String> books = new HashMap<>();
@@ -87,6 +121,11 @@ public class BookManager {
         return books;
     }
 
+    /**
+     * check inventory size by ISBN
+     * @param book_ISBN book ISBN
+     * @return an integer indicating inventory size of the corresponding ISBN
+     */
 
     public int checkInventory(String book_ISBN) {
         ArrayList<Integer> book_searched = searchBook(book_ISBN);
@@ -104,6 +143,12 @@ public class BookManager {
         return book_unlended.size();
     }
 
+    /**
+     * To check return date of book with id bookID
+     * @param bookID book ID
+     * @return LocalDate of the return date of a book with ID bookID
+     */
+
 
     public LocalDate checkReturnDate(int bookID) {
         LocalDate d = null;
@@ -115,6 +160,12 @@ public class BookManager {
         return d;
     }
 
+    /**
+     * To modify the return date of book with id bookID to desire_date
+     * @param bookID book ID
+     * @param desire_date new return date
+     */
+
     public void changeReturnDate(int bookID, LocalDate desire_date){
         for (Book book : allBooks) {
             if (book.getBookID() == bookID) {
@@ -122,6 +173,12 @@ public class BookManager {
             }
         }
     }
+
+    /**
+     * To change the status of a book with id bookID to a new status
+     * @param bookID book ID
+     * @param status book Status
+     */
 
     public void changBookStatus(int bookID, BookPositionStatus status){
         for (Book book : allBooks) {
