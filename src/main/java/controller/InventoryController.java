8bd8f97cdf52f560568c93cfd313a4030b314a8b
nzhou27 <zhouhanzhi0818@gmail.com>
2021-11-14 22:01:56 -0500

    InventoryController.java implemented

--- src/main/java/controller/InventoryController.java
+++ src/main/java/controller/InventoryController.java
@@ -1,20 +1,30 @@
 package controller;
 
 import Book.Book;
+import MongoDBGateway.IMongoDBBookMethods;
+import MongoDBGateway.MongoDBBookMethods;
 import UseCase.DBbookManager;
+import UseCase.IDBbookManager;
 
 import java.time.LocalDate;
 
-//
-//public class InventoryController {
-//    //TODO : ADD A IMONGODBBOOK METHODS PARAMETER TO THE INPUT
-//
-//    public void addBookToInventory(int bookID, String bookName, String ISBN, LocalDate publishDate, String author, String type){
-////        if type.equals("m"){
-////    }
-////    }
-//
-////    public boolean deleteBookFromInventory(int bookID) {
-//////        return DBbookManager.deleteBook(bookID);
-////
-////    }
+
+public class InventoryController {
+
+    public void addBookToInventory(int bookID, String bookName, String ISBN, LocalDate publishDate,
+                                   String author, String type){
+        IDBbookManager b = new DBbookManager();
+        IMongoDBBookMethods bm = new MongoDBBookMethods();
+        Book book = new Book(bookID, bookName, ISBN, publishDate, author, type);
+        b.addBook(book, bm);
+
+    }
+
+
+    public void deleteBookFromInventory(int bookID) {
+        IDBbookManager b = new DBbookManager();
+        IMongoDBBookMethods bm = new MongoDBBookMethods();
+        b.deleteBook(bookID, bm);
+
+
+    }}
