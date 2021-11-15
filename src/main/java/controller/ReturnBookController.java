17c0e458275e73537c43947f9f6eb0da69374b9d
nzhou27 <zhouhanzhi0818@gmail.com>
2021-11-14 21:57:42 -0500

    ReturnBookController.java implemented

--- src/main/java/controller/ReturnBookController.java
+++ src/main/java/controller/ReturnBookController.java
@@ -1,16 +1,25 @@
 package controller;
 
 import Book.BookCondition;
+import MongoDBGateway.IMongoDBBookMethods;
+import MongoDBGateway.IMongoDBStudentMethods;
+import MongoDBGateway.MongoDBBookMethods;
+import MongoDBGateway.MongoDBStudentMethods;
+import UseCase.*;
 
 public class ReturnBookController {
 
 
 
-    public void returnBook(int bookID, BookCondition BookCondition, String username){
+    public void returnBook(int bookID, BookCondition BookCondition){
+        IDBbookManager b = new DBbookManager();
+        IMongoDBBookMethods bm = new MongoDBBookMethods();
+        if (BookCondition == Book.BookCondition.GOOD){
+            b.changBookStatus(bookID, BookPositionStatus.UNLENDED, bm);
 
-    }
+        }
+        else if (BookCondition == Book.BookCondition.DAMAGE){
+            b.deleteBook(bookID, bm);
+        }
 
-    public void changeCreditScore(String username, int change_by) {
-
-    }
-}
+    }}
