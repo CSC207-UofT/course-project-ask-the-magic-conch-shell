a4f9574e749151d2039e2beb0ab21fb590698fdc
MAC <shangqi.zhang@mail.utoronto.ca>
2021-12-05 17:22:39 -0500

    changeStudentPassword demo1

--- src/main/java/com/bookSystem/controller/changeStudentPassword.java
+++ src/main/java/com/bookSystem/controller/changeStudentPassword.java
@@ -27,24 +27,37 @@ public class changeStudentPassword {
         @Autowired
         private IMongoDBStudentMethods sm;
 
-        @Autowired
-        private IMongoDBStaffMethods sam;
+        @GetMapping
+        public String loadChangeStudentPassword() {
+
+            return "changeStudentPassword";
+        }
 
         @PostMapping("")
-        public String changeStudentPassword(@RequestParam(defaultValue = "null", name = "user_type") String user_type, @RequestParam("username") String username,
-                                            @RequestParam("oldPassword") String Opassword, @RequestParam("newPassword") String Npassword, Model model) {
-            if (!Objects.equals(user_type, "null")) {
-                if (Objects.equals(user_type, "student")) {
-                    if (sm.checkStudent(username)
-                            && Objects.equals(Opassword, um.studentDBGetPassword(username, sm))) {
-                        um.studentDBModifyPassword(username, Opassword, Npassword, sm);
-                        model.addAttribute("message", "Change successfully!");
-                        return "changeStudentPassword";
-                    }
-
+        public String changeStudentPassword(@RequestParam(defaultValue = "null", name = "user_type") String user_type,
+                                            @RequestParam("username") String username,
+                                            @RequestParam("oldPassword") String Opassword,
+                                            @RequestParam("new_password") String Npassword1,
+                                            @RequestParam("new_password2") String Npassword2, Model model) {
+            if (Objects.equals(user_type, "student")) {
+                if (sm.checkStudent(username)
+                        && Objects.equals(Opassword, um.studentDBGetPassword(username, sm))
+                        && Npassword1 == Npassword2) {
+                    um.studentDBModifyPassword(username, Opassword, Npassword1, sm);
+                    model.addAttribute("message", "Change successfully!");
+                    return "changeStudentPassword";
                 }
+                else if (!Objects.equals(Opassword, um.studentDBGetPassword(username, sm))){
+                    model.addAttribute("message", "Old password does not match!");
+                    return "changeStudentPassword";
+                }
+                else if (Npassword1 != Npassword2) {
+                    model.addAttribute("message", "New passwords do not match!");
+                    return "changeStudentPassword";
+                }
+            return "changeStudentPassword";
 
-            }
+        }
             return "changeStudentPassword";
         }
     }}
