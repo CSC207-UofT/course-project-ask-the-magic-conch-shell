265628662db118a5b3dbef55597196acda2c1479
nzhou27 <zhouhanzhi0818@gmail.com>
2021-12-06 01:36:56 -0500

    extendReturnDateController.java implemented

--- src/main/java/com/bookSystem/controller/extendReturnDateController.java
+++ src/main/java/com/bookSystem/controller/extendReturnDateController.java
@@ -1,13 +1,18 @@
 package com.bookSystem.controller;
 
 
+import com.bookSystem.mongoDBGateway.IMongoDBBookMethods;
+import com.bookSystem.useCase.IDBbookManager;
 import com.bookSystem.useCase.IUserLoginManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDate;
 
 @Controller
 @RequestMapping("extend")
@@ -15,6 +20,12 @@ public class extendReturnDateController {
 
     @Autowired
     private IUserLoginManager ulm;
+
+    @Autowired
+    private IDBbookManager bm;
+
+    @Autowired
+    private IMongoDBBookMethods mbm;
 
     @GetMapping
     public String loadExtend() {
@@ -27,8 +38,16 @@ public class extendReturnDateController {
 
 
     @PostMapping
-    public String extendDate(@RequestParam("bookidToExtend") int book_id){
+    public String extendDate(@RequestParam("bookidToExtend") int book_id, Model model){
         if(ulm.getCurrentStudent().getCreditScore() >=90 ){
+            bm.changeReturnDate(book_id, LocalDate.now().plusDays(7), mbm);
+            model.addAttribute("message", "Your return date is extended to"+
+                    LocalDate.now().plusDays(7));}
+
+        else{
+            model.addAttribute("message", "Sorry, your credit score is not high enough to" +
+                    "extend return date");
+
 
 
         }
